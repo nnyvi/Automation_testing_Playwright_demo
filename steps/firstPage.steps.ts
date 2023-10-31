c1a75418060cecd827479bdb5c6763e2c1118848
nnyvi <149259509+nnyvi@users.noreply.github.com>
2023-10-31 10:59:33 +0700

    Update firstPage.steps.ts

--- steps/firstPage.steps.ts
+++ steps/firstPage.steps.ts
@@ -5,17 +5,17 @@ import { page } from "../hooks/hooks";
 Given('User accesses on the Google homepage', async() => {
     await expect(page.locator("//img[@alt='Google']")).toBeVisible();
 });
-When('user fills search information', async() => {
+When('User fills search information', async() => {
     const input = await page.locator("//*[@class='gLFyf']");
     await input.click();
     await input.fill("Apple");
 });
-When('user click "Xem trang đầu tiên tìm được" button', async() => {
+When('User click "Xem trang đầu tiên tìm được" button', async() => {
     const button = await page.getByRole('button', {name: "Xem trang đầu tiên tìm được"});
     await button.click();
 });
 
-Then('user accesses on the first searched page', async() => {
+Then('User accesses on the first searched page', async() => {
     await page.waitForSelector("//body[contains(@class, 'page-home')]");
     await expect(page).toHaveURL("https://www.apple.com/vn/");
 });
