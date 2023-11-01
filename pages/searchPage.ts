d850435155590af6191b16fddb5a7a684f6c7c98
nieuvi <126230027+nieuvi@users.noreply.github.com>
2023-11-01 11:35:49 +0700

    Update searchPage.ts add path

--- pages/searchPage.ts
+++ pages/searchPage.ts
@@ -46,7 +46,7 @@ export class SearchPage {
         } else if (fileChooserEvent !== null) {
             const fileChooser = fileChooserEvent as FileChooser;
             // Use fileChooser as normal
-            await fileChooser.setFiles('C:\Users\intern.nnyvi\demo\img\test.jpg');
+            await fileChooser.setFiles('C:\Users\intern.nnyvi\Playwright_BDD_demo\img\test.jpg');
         } else {
             // return notice when not receiving filechooser
             return (`Error: No file chooser found`);
